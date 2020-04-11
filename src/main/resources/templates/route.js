00b12a1d166f4b5bad2df56f0f073ed8635d8825
Barata Estevao <barataestevaomario@gmail.com>
2020-04-11 12:35:08 -0700

    mess

---
+++ src/main/resources/templates/route.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+//var run = require(__dirname + "/astroport.js");
+
+router.get("/", function(request, response){
+	var shipname = request.param("ship");
+	var ship = "";
+	if(shipname !== undefined)
+		ship = shipname;
+	response.render("templates/astroport", {ship: ship});
+	response.setHeader("Content-Type", "text/html");
+});
+
+module.exports = router;
